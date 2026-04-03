011c65f7ddb14e282d2ee4cf571efb6073b5b541
GustaKowai <gusta.kowai@gmail.com>
2026-04-03 16:50:16 -0300

    Barra de vida do inimigo, da base, inimigo segue a base

--- gd/EnemyFollow.gd
+++ gd/EnemyFollow.gd
@@ -4,10 +4,10 @@ class_name  EnemyFollow
 @export var enemy: Enemy
 @export var move_speed:float = 30.0
 
-var player: CharacterBody2D
+@export var base: StaticBody2D
 
 func Enter():
-	player = get_tree().get_first_node_in_group("Player")
+	base = get_tree().get_first_node_in_group("Base")
 
 func Exit():
 	pass
@@ -16,12 +16,14 @@ func Update(_delta: float):
 	pass
 	
 func Physics_update(_delta: float):
-	var direction = player.global_position - enemy.global_position
-	print_debug(direction.length())
-	
-	if direction.length() > 25:
-		enemy.velocity = direction.normalized()*move_speed
+	if base:
+		var direction = base.global_position - enemy.global_position
+		print_debug(direction.length())
+		if direction.length() > 25:
+			enemy.velocity = direction.normalized()*move_speed
+		else:
+			enemy.velocity = Vector2()
+		#if direction.length() > 500:
+			#Transitioned.emit(self,"idle")
 	else:
-		enemy.velocity = Vector2()
-	if direction.length() > 500:
 		Transitioned.emit(self,"idle")
--- gd/base.gd
+++ gd/base.gd
@@ -1,23 +1,20 @@
-extends CharacterBody2D
-class_name Enemy
-@onready var progress_bar: ProgressBar = $ProgressBar
+extends StaticBody2D
+class_name Base
 
-@export var max_health:int = 10
+@onready var progress_bar: ProgressBar = $ProgressBar
+var max_health:int = 100
 var current_health: int :
 	set(new_value):
 		current_health = new_value
 		progress_bar.value = new_value
-
+		
 func _ready() -> void:
 	current_health = max_health
 
-func _physics_process(delta: float) -> void:
-	move_and_slide()
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if !velocity.is_zero_approx():
-		look_at(global_position+velocity)
-
+	pass
 
 func take_damage(damage:int):
 	current_health -= damage
