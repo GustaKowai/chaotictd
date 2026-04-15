f0ea8c595ce1dda8f09d09b5f8261f650b9024f1
GustaKowai <gusta.kowai@gmai.com>
2026-04-15 15:32:18 -0300

    Adicionada a bala bumerangue

---
+++ gd/bullet_bumerangue.gd
@@ -0,0 +1,22 @@
+extends Bullet
+@export var bullet_duracao = 0.48
+var bullet_tempodevida = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT_IN)
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self,"speed",-speed, bullet_duracao)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	bullet_tempodevida += delta
+	if bullet_tempodevida > bullet_duracao:
+		queue_free()
+
+func _on_body_entered(body):
+	#print_debug(body)
+	if body.is_in_group("Enemy"):
+		body.take_damage(damage)
