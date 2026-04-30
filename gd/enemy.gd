a0fdb006c5f0a83c5512c50a5195b060bcc1bd13
GustaKowai <gusta.kowai@gmai.com>
2026-04-30 13:20:06 -0300

    Inimigos agora podem dropar peças (ainda não implementadas), piscam em alguma cor ao levar dano e podem ter uma animação de morte (ainda não implementada)

--- gd/enemy.gd
+++ gd/enemy.gd
@@ -6,9 +6,17 @@ class_name Enemy
 
 @onready var progress_bar: ProgressBar = $ProgressBar
 @onready var sprite: Sprite2D = $Sprite
-
-@export var distancia_colisao:float
+@export_category("Life")
+#@export var distancia_colisao:float
 @export var max_health:int = 10
+@export var death_prefab:PackedScene
+@export var color_damage:Color
+@export_category("pieces")
+@export var pieces:PackedScene
+@export_range(0,1) var pieces_rate:float = 0.5
+@export var number_pieces_min:int = 1
+@export var number_pieces_max:int = 1
+@export var spread_area_radius:float = 10.0
 
 var alvo:Building  #Utilizado para inimigos que podem mudar de alvo
 
@@ -31,8 +39,16 @@ func _process(delta: float) -> void:
 
 func take_damage(damage:int):
 	current_health -= damage
+	blink_damage()
 	if current_health <= 0:
 		die()
+
+func blink_damage():
+	modulate = color_damage
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(self,"modulate",Color.WHITE,0.3)
 
 func heal_damage(heal:int):
 	if current_health+heal < max_health:
@@ -41,4 +57,23 @@ func heal_damage(heal:int):
 		current_health = max_health
 		
 func die():
+	if death_prefab:
+		var death_object = death_prefab.instantiate()
+		death_object.position = position
+		get_parent().add_child(death_object)
+	await drop_pieces()
 	queue_free()
+	
+	
+func drop_pieces():
+	if not pieces:
+		#print ("Não dropo peças")
+		return
+	if randf() > pieces_rate: return #Checa se ele dropará um item baseado na taxa de drop do monstro
+	var number_pieces = randi_range(number_pieces_min,number_pieces_max)
+	for n in range(1,number_pieces):
+		var position_spread:Vector2 = Vector2(randf_range(-1,1),randf_range(-1,1))*spread_area_radius
+		#print_debug(position_spread)
+		var item = pieces.instantiate()
+		item.position = position+position_spread
+		get_parent().add_child(item)
