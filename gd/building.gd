b77bbd279093b4fb99f3cf15f7b63cbcd6696b7e
GustaKowai <gusta.kowai@gmai.com>
2026-05-06 00:42:33 -0300

    Adicionado escudo nas torres e a capacidade de ganhar esse escudo.

--- gd/building.gd
+++ gd/building.gd
@@ -6,6 +6,10 @@ class_name Building
 @export var collision_shape:CollisionShape2D
 @export var max_health:int = 100
 @export var value:int
+var shield:int:
+	set(new_value):
+		current_health = new_value
+		#progress_bar.value = new_value #Se for usar uma barra de escudo também, sendo em baixo ou sobre a vida
 var current_health: int :
 	set(new_value):
 		current_health = new_value
@@ -21,6 +25,9 @@ func _process(delta: float) -> void:
 	pass
 
 func take_damage(damage:int):
+	if shield > 0:
+		damage_to_shield(damage)
+		return
 	current_health -= damage
 	if current_health <= 0:
 		die()
@@ -33,3 +40,22 @@ func heal_damage(heal:int):
 
 func die():
 	queue_free()
+	
+func damage_to_shield(amount:int):
+	if amount < shield:
+		shield -= amount
+		#modulate = Color.CYAN
+		#var tween = create_tween()
+		#tween.set_ease(Tween.EASE_IN)
+		#tween.set_trans(Tween.TRANS_QUINT)
+		#tween.tween_property(self,"modulate",Color.WHITE,0.3)
+		#print_debug(player_shield)
+	else:
+		amount -= shield
+		shield = 0
+		take_damage(amount)
+func add_shield(amount:int):
+	shield += amount
+	
+func set_shield(amount:int):
+	shield = amount
