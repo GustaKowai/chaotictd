25170cd8916d1859aa85e57b351003bc31910144
mikaralho-aa <lipe.fe.gregorio@gmail.com>
2026-05-17 21:04:16 -0300

    slow bullet start

---
+++ slow_bullet.gd
@@ -0,0 +1,11 @@
+extends Bullet
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+	
+func _on_area_entered(body):
+	#print_debug(body)
+	if body.is_in_group("Enemy"):
+		var target:Enemy = body.get_parent()
+		target.take_damage(damage)
+		hit()
