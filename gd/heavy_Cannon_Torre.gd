0dcca39ec5d4519461d602ce0e9544e7b16608ce
GustaKowai <gusta.kowai@gmai.com>
2026-05-19 11:35:29 -0300

    heavy cannon animado

--- gd/heavy_Cannon_Torre.gd
+++ gd/heavy_Cannon_Torre.gd
@@ -4,24 +4,34 @@ class_name Heavy_Cannon_Torre
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var mira: Marker2D = %Mira
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 @export var delay_shoot: float = 5 #Delay do tiro do canhao
 @export var bullet: PackedScene
 var delay: float
 
+func _ready() -> void:
+	super()
+	animation_player.speed_scale = 7.5/delay_shoot
+
 func _process(delta: float) -> void:
 	sprite_2d.look_at(get_global_mouse_position())
 
 func _physics_process(delta: float) -> void:
-	delay -= delta
-	if delay <= 0:
-		shoot()
-		delay = delay_shoot
-
+	#delay -= delta
+	#if delay <= 0:
+		#shoot()
+		#delay = delay_shoot
+	pass
+	
 func shoot():
+	print_debug(animation_player.speed_scale)
 	var b = bullet.instantiate()
 	get_tree().root.add_child(b)
 	b.transform = mira.global_transform
 	
 func _on_rota_1_1_pressed() -> void:
 	pass # Replace with function body.
+
+func charge():
+	animation_player.play("carregar")
