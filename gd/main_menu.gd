773ed005fc759a26dba3ff940808202bd2757186
RenanFGSimoes <renanfgs@estudante.ufscar.br>
2026-06-16 11:11:33 -0300

    Tela inicial

---
+++ gd/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/StartGame.pressed.connect(_on_start_game_pressed)
+	$VBoxContainer/Options.pressed.connect(_on_options_pressed)
+	$VBoxContainer/Scoreboard.pressed.connect(_on_scoreboard_pressed)
+	$VBoxContainer/ExitGame.pressed.connect(_on_exit_game_pressed)
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://cenas/main.tscn")
+
+func _on_options_pressed():
+	pass
+
+func _on_scoreboard_pressed():
+	pass
+	
+func _on_exit_game_pressed():
+	get_tree().quit()
