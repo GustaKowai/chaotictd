3a97f87d01789be6310843fd00bd7b652e53f26e
GustaKowai <gusta.kowai@gmai.com>
2026-06-25 00:52:55 -0300

    tentando brincar com glow

--- gd/area_dano.gd
+++ gd/area_dano.gd
@@ -4,6 +4,7 @@ extends Area2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 var retira_cammo:bool = false
 var upgrade_money:bool = false
+var tempo:float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,16 +12,18 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func _process(delta):
+	tempo += delta
 
 
 func _on_timer_timeout():
+	print_debug("acabou o tempo ",tempo)
 	causar_dano()
 	
 func causar_dano():
 	animated_sprite_2d.play("chock")
-	#print_debug("tentou_causar_dano")
+	get_parent().blink_damage(Color(1.476, 1.476, 0.0, 1.0))
+	print_debug("tentou_causar_dano")
 	var inimigos = get_overlapping_areas()
 	#print_debug(inimigos)
 	for inimigo in inimigos:
