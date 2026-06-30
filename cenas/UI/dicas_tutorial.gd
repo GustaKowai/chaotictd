51bcde4968cdfa706629b06313a3c859277d23e1
GustaKowai <gusta.kowai@gmai.com>
2026-06-30 17:49:15 -0300

    Ajuste no buraco negro e ajuste no tutorial

--- cenas/UI/dicas_tutorial.gd
+++ cenas/UI/dicas_tutorial.gd
@@ -2,6 +2,8 @@ extends PanelContainer
 @onready var texto: RichTextLabel = $RichTextLabel
 @onready var timer: Timer = $RichTextLabel/Timer
 @export_multiline() var tutorial:Array[String]
+@export var menu_torres:PanelContainer
+@export var botao_auto:CheckButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +13,7 @@ func show_message(wave):
 	match wave:
 		0:
 			choose_tutorial(0)
+			blink(menu_torres)
 		2:
 			choose_tutorial(1)
 		5: 
@@ -19,6 +22,7 @@ func show_message(wave):
 			choose_tutorial(3)
 		3:
 			choose_tutorial(4)
+			blink(botao_auto)
 func hide_tutorial():
 	hide()
 
@@ -35,3 +39,12 @@ func _on_timer_timeout() -> void:
 func _on_button_pressed() -> void:
 	timer.stop()
 	hide_tutorial()
+
+func blink(UI):
+	print_debug("tentei piscar")
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.set_loops(5)
+	tween.tween_property(UI,"modulate",Color(2.313, 2.313, 2.313, 1.0),1)
+	tween.tween_property(UI,"modulate",Color.WHITE,1)
